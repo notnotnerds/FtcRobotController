c871209f7c20d96a73426ce61a4c3b8d4f90191d
Stephan Kruglov <skruglov.2023@carmelhs.org>
2020-09-28 16:00:55 -0500

    Code update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name="Robot Driving Practice, beginner level", group="drivers")
 public class DrivingPractice   extends LinearOpMode {
@@ -17,26 +18,28 @@ public class DrivingPractice   extends LinearOpMode {
         telemetry.addLine("To move forward, use the right joystick y");
         telemetry.addLine("To rotate, use the right joystick x");
         telemetry.addLine("To strafe side to side, use the left joystick x");
-        telemetry.addLine("For now, you will be limited to quarter power. When you get better, you will be given the option to advance");
+        telemetry.addLine("For now, you will be limited to half power. When you get better, you will be given the option to advance");
         telemetry.update();
 
         fl=hardwareMap.dcMotor.get("fl");
         fr=hardwareMap.dcMotor.get("fr");
         bl=hardwareMap.dcMotor.get("bl");
         br=hardwareMap.dcMotor.get("br");
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        bl.setDirection(DcMotor.Direction.REVERSE);
 
 
 
-        double m=.5; //speed multiplier
+        double m=.3; //speed multiplier
 
         waitForStart();
         while (opModeIsActive()) {
             double drive=gamepad1.right_stick_y;
             double strafe=gamepad1.left_stick_x;
             double rotate=gamepad1.right_stick_x;
-            fl.setPower(m * (drive + strafe + rotate));
+            fl.setPower(m * (-drive + strafe + rotate));
             bl.setPower(m * (drive - strafe + rotate));
-            fr.setPower(m * (drive - strafe - rotate));
+            fr.setPower(m * (-drive - strafe - rotate));
             br.setPower(m * (drive + strafe - rotate));
         }
     }
