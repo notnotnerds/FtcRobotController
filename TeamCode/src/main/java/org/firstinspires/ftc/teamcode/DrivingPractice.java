7afade35474695522b8df523633c91162e3a9abf
Stephan Kruglov <skruglov.2023@carmelhs.org>
2020-10-05 15:56:42 -0500

    TeleOp update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
@@ -26,22 +26,31 @@ public class DrivingPractice   extends LinearOpMode {
         fr=hardwareMap.dcMotor.get("fr");
         bl=hardwareMap.dcMotor.get("bl");
         br=hardwareMap.dcMotor.get("br");
-        fl.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.REVERSE);
         bl.setDirection(DcMotor.Direction.REVERSE);
 
-
-
-        double m=.5; //speed multiplier
+        boolean back=false;
+        double m=.25; //speed multiplier
 
         waitForStart();
         while (opModeIsActive()) {
+            if(gamepad1.start){
+                if(back==false){
+                    back=true;
+                    m=5;
+                }
+                else if(back==true){
+                    back=false;
+                    m=.25;
+                }
+            }
             double drive=gamepad1.right_stick_y;
             double strafe=gamepad1.left_stick_x;
             double rotate=gamepad1.right_stick_x;
-            bl.setPower(m * (gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x));
-            br.setPower(m * (gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x));
-            fr.setPower(m * (-gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x));
-            fl.setPower(m * (-gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x));
+            bl.setPower(m * (gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x));
+            br.setPower(m * (gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x));
+            fr.setPower(m * (gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x));
+            fl.setPower(m * (gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x));
         }
     }
 }
