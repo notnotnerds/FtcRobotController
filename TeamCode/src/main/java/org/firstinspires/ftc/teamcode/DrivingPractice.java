b7194c8246055aa0915308854915a65c1bea24db
Stephan Kruglov <skruglov.2023@carmelhs.org>
2020-10-06 19:12:47 -0500

    Code update, more or less driveable robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+//Code property of team 12051 NotNotNerds. Please do not just take this for your own use
 @TeleOp (name="Robot Driving Practice, beginner level", group="drivers")
 public class DrivingPractice   extends LinearOpMode {
     public DcMotor fl;
@@ -19,7 +19,8 @@ public class DrivingPractice   extends LinearOpMode {
         telemetry.addLine("To rotate, use the right joystick x");
         telemetry.addLine("To strafe side to side, use the left joystick x");
         telemetry.addLine("For now, you will be limited to half power. When you get better, you will be given the option to advance");
-        telemetry.addLine("Don't crash the robot! Or the coders will be furious");
+        telemetry.addLine("Do NOT crash the robot! The coders will be furious and will forever keep you at 10% power while they enjoy their turbo boost");
+        telemetry.addLine("DANNY, you will not be using turbo speed!");
         telemetry.update();
 
         fl=hardwareMap.dcMotor.get("fl");
@@ -34,7 +35,7 @@ public class DrivingPractice   extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            if(gamepad1.start){
+            if(gamepad1.back){//the area that drives danny crazy
                 if(back==false){
                     back=true;
                     m=5;
@@ -44,13 +45,10 @@ public class DrivingPractice   extends LinearOpMode {
                     m=.25;
                 }
             }
-            double drive=gamepad1.right_stick_y;
-            double strafe=gamepad1.left_stick_x;
-            double rotate=gamepad1.right_stick_x;
-            bl.setPower(m * (gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x));
-            br.setPower(m * (gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x));
-            fr.setPower(m * (gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x));
-            fl.setPower(m * (gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x));
+            bl.setPower(m * (gamepad1.right_stick_x - gamepad1.right_stick_y)+gamepad1.left_stick_x);
+            br.setPower(m * (gamepad1.right_stick_x + gamepad1.right_stick_y)+gamepad1.left_stick_x);
+            fr.setPower(m * (gamepad1.right_stick_x - gamepad1.right_stick_y)+gamepad1.left_stick_x);
+            fl.setPower(m * (gamepad1.right_stick_x + gamepad1.right_stick_y)+gamepad1.left_stick_x);
         }
     }
 }
