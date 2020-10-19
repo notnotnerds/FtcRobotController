5e6df27b478c262c7ffb490d1d432d3e5dc0b1e7
Stephan Kruglov <skruglov.2023@carmelhs.org>
2020-10-18 23:59:00 -0500

    Removed compile error in annapractice, added opencv and control hub mod code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnnaPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnnaPractice.java
@@ -1,14 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp
-public class AnnaPractice extends OpMode {
-    @Override
+@Autonomous
+public class AnnaPractice extends LinearOpMode {
+    /*@Override
     public void init() {
-        telementry.addData("Hello", "World");
+        telemetry.addData("Hello", "World");
+    } //commented out because compile errors
+    */
+
+    @Override
+    public void runOpMode()  {
+       //initialization of field
+        waitForStart();
+        while (opModeIsActive()) {
+           //do nothing
+        }
     }
-
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingPractice.java
@@ -5,7 +5,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
